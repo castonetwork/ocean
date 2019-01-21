968684c5a7b1e12cef5167e708292e8634aa946f
hhan <hhan87@gmail.com>
2019-01-21 18:24:54 +0900

    impl: marker and route removed when peer:disconnect occurred

--- src/setupNode.js
+++ src/setupNode.js
@@ -11,27 +11,73 @@ const radarIcon = L.icon({
   popupAnchor: [-3, -14]
 });
 
-const addPeerMarker = ({map, latitude, longitude, name}) =>
-  L.marker([latitude, longitude], {
+const peerMarkerMap = {};
+const peerRouteMap = {};
+window.peerMarkerMap = peerMarkerMap;
+window.peerRouteMap = peerRouteMap;
+const addMarkerMap  = (peerId, obj, latitude, longitude) =>{
+  if(!peerMarkerMap[peerId]) peerMarkerMap[peerId] = {};
+  peerMarkerMap[peerId].marker = obj;
+  peerMarkerMap[peerId].latitude = latitude;
+  peerMarkerMap[peerId].longitude = longitude;
+}
+const addRouteMap = (fromId, toId, obj)=>{
+      peerRouteMap[fromId+"|"+toId] = obj;
+  // if(peerMarkerMap[fromId] && peerMarkerMap[toId] &&
+  //   peerMarkerMap[fromId].latitude && peerMarkerMap[fromId].longitude &&
+  //   peerMarkerMap[toId].latitude && peerMarkerMap[toId].longitude){}
+}
+
+const removeRouter  = (peerId) =>{
+  let filtered = Object.entries(peerRouteMap).filter(arr => arr[0].indexOf(peerId) > -1);
+  filtered.forEach(o=> o[1].remove());
+  filtered.forEach(o=> delete peerRouteMap[o[0]]);
+
+}
+
+const addPeerMarker = ({peerId, map, latitude, longitude, name}) =>{
+  if(!latitude || !longitude){
+    return;
+  }
+  let marker = L.marker([latitude, longitude], {
     title: name,
     icon: radarIcon
   }).addTo(map);
-const addPeerRoutes = ({map, latitude, longitude, coords}) =>
-  L.curve(
+  addMarkerMap(peerId, marker, latitude, longitude);
+  return marker;
+}
+const addPeerRoutes = ({fromId, toId, map, latitude, longitude, coords}) => {
+  if(!coords || !latitude || !longitude || !coords.latitude || !coords.longitude){
+    return;
+  }
+  let route = L.curve(
     [
       "M",
-      [latitude, longitude],
+      [+latitude, +longitude],
       "Q",
       [
-        latitude + Math.abs(longitude - coords.longitude) * 0.7,
-        longitude - (longitude - coords.longitude) * 0.2
+        +latitude + Math.abs(longitude - coords.longitude) * 0.7,
+        +longitude - (longitude - coords.longitude) * 0.2
       ],
-      [coords.latitude, coords.longitude],
+      [+coords.latitude, +coords.longitude],
     ],
     {
       color: "#ea8080"
     }
   ).addTo(map);
+  addRouteMap(fromId, toId, route);
+  return route;
+}
+
+const addRouter = (fromId, toId, map)=>{
+  let from = peerMarkerMap[fromId];
+  let to = peerMarkerMap[toId];
+  addPeerRoutes({fromId, toId, map, latitude: from.latitude, longitude: from.longitude, coords:{
+    latitude: to.latitude,
+    longitude: to.longitude
+  }})
+
+}
 /* for Debug */
 window.addPeerMarker = addPeerMarker;
 window.addPeerRoutes = addPeerRoutes;
@@ -42,6 +88,7 @@ const setupNode = async ({node, serviceId}) => {
     [0, 180],
     3
   );
+  window.map = map;
   const dialToPrism = peerInfo =>
     node.dialProtocol(peerInfo, `/prism/${serviceId}/info`, async (err, conn) => {
       if (err) {
@@ -56,24 +103,36 @@ const setupNode = async ({node, serviceId}) => {
         pull.take(o => o.topic !== DIAL_TERMINATED),
         pull.drain(event => {
           const events = {
-            'prismInfo': async ({data: {coords, flows, waves}}) => {
+            'initPrismInfo': async ({data: {coords, flows, waves}}) => {
+              if(!coords) return;
               // add a marker of prism
-              addPeerMarker({map, latitude: coords.latitude, longitude: coords.longitude, name: "prism"});
+              addPeerMarker({peerId: idStr, map, latitude: coords.latitude, longitude: coords.longitude, name: "prism"});
               // add markers of flows
               flows && Object.entries(flows).filter(([id, obj])=>obj.coords)
                 .forEach(([id, {coords: {latitude: flowLatitude, longitude: flowLongitude}, waves: waveIds}])=> {
-                  addPeerMarker({map, latitude: flowLatitude, longitude: flowLongitude, name: id });
-                  addPeerRoutes({map, latitude: flowLatitude, longitude: flowLongitude, coords})
+                  addPeerMarker({peerId: id, map, latitude: flowLatitude, longitude: flowLongitude, name: id });
+                  addPeerRoutes({fromId: id, toId: idStr, map, latitude: flowLatitude, longitude: flowLongitude, coords})
                     ._path.classList.add('flows');
-                  waveIds && Object.entries(waveIds).map(([id])=>waves[id].coords)
-                    .forEach(waveCoords => addPeerRoutes({map, latitude: coords.latitude, longitude: coords.longitude, coords: waveCoords}));
+                  waveIds && Object.entries(waveIds).map(([id])=>({id, waveCoords: waves[id].coords}))
+                    .forEach(({id, waveCoords}) => addPeerRoutes({fromId: idStr, toId: id, map, latitude: coords.latitude, longitude: coords.longitude, coords: waveCoords}));
                 }
               );
               waves && Object.entries(waves).filter(([id, obj])=> obj.coords && obj.coords.latitude && obj.coords.longitude)
                 .forEach(([id, {coords: {latitude: waveLatitude, longitude: waveLongitude}}])=> {
-                  addPeerMarker({map, latitude: waveLatitude, longitude: waveLongitude, name: id})
+                  addPeerMarker({peerId: id, map, latitude: waveLatitude, longitude: waveLongitude, name: id})
                 })
             },
+            'addPeerMarker': ({peerId, coords})=>{
+              addPeerMarker({peerId, map, latitude: coords.latitude, longitude: coords.latitude, name: peerId });
+
+            },
+            'addRouter': ({fromId, toId})=>{
+              addRouter(fromId, toId, map);
+
+            },
+            'removeRouter': ({peerId})=>{
+              removeRouter(peerId);
+            }
           };
           if (events[event.topic]) return events[event.topic](event);
           else {
@@ -102,6 +161,11 @@ const setupNode = async ({node, serviceId}) => {
     const disconnPeerId = peerInfo.id.toB58String();
     if (disconnPeerId && peers[disconnPeerId]) {
       peers[disconnPeerId].isDialed = false;
+    }
+    if(peerMarkerMap[disconnPeerId]){
+      peerMarkerMap[disconnPeerId].marker.remove();
+      delete peerMarkerMap[disconnPeerId];
+      removeRouter(disconnPeerId);
     }
   });
   node.start(err => {
